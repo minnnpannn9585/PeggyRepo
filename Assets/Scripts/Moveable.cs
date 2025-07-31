21e30ef2bce29176e82b0faf30ffc0bdecbd5448
peggy404notfound <952025824@qq.com>
2025-08-01 01:16:13 +0800

    0801

--- Assets/Scripts/Moveable.cs
+++ Assets/Scripts/Moveable.cs
@@ -11,43 +11,31 @@ public class Moveable : MonoBehaviour
     public float speed = 1.0f;
     private Vector3 target;
 
-    bool isGoingToLight = false;
-    public Transform lightTrans;
-
     private void Start()
     {
         //target = end.position;
         target = points[index].position;
     }
 
+
     private void Update()
     {
-        if (!isGoingToLight)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
 
-            if (Vector3.Distance(transform.position, target) < 0.1f)
+        if (Vector3.Distance(transform.position, target) < 0.1f)
+        {
+
+            index++;
+            if (index < points.Length)
+                transform.forward = points[index].position - points[index - 1].position;
+            if (index == points.Length)
             {
-
-                index++;
-                if (index < points.Length)
-                    transform.forward = points[index].position - points[index - 1].position;
-                if (index == points.Length)
-                {
-                    index = 0;
-                    transform.forward = points[index].position - points[points.Length - 1].position;
-                }
-                target = points[index].position;
+                index = 0;
+                transform.forward = points[index].position - points[points.Length - 1].position;
             }
+            target = points[index].position;
         }
 
-        else
-        {
-            transform.position = Vector3.MoveTowards(transform.position, lightTrans.position, speed * Time.deltaTime);
-        }
 
-        
-
-        
     }
 }
